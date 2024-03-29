33793b9b26dac8e6f6f6d1a887ff215827e73c94
Vamshi5454 <ponugotivamshi007@gmail.com>
2024-03-29 10:05:36 -0500

    updated connecting icons

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -65,7 +65,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/Vamshi5454"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -85,7 +85,7 @@ function Home2() {
               </li> */}
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="linkedin.com/in/ponugoti-vamshi-323328167/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
